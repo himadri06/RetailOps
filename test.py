3d2769c263dc8c1fa43ead9dbedfc89d86df0519
himadri06 <hbhowmick91@gmail.com>
2026-04-30 11:54:36 +0530

    started development

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-#samplecode
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("test").getOrCreate()
+df=spark.createDataFrame([(1,"Himadri"),(2,"data")],["id","name"])
+df.show()
